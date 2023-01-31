4967ad44ab648b1199da4b433cf6c775a1147af1
Lougam <108102295+Lougam@users.noreply.github.com>
2023-01-31 18:35:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,58 +1,101 @@
 
+
 d = new Date();
 
 hour = d.getHours();
+day = d.getDay();
 
-if(hour > 23 && hour <= 2)
+if(day < 1 && day >= 6)
 {
-document.getElementById("status").src="IMAGENS/hub14.png"
+    document.getElementById("status").src="IMAGENS/.png"  
 }
 
 else
-if(hour > 2 && hour <= 4)
+if(hour >= 24 && hour < 2)
 {
-document.getElementById("status").src="IMAGENS/hub08.png"
+document.getElementById("status").src="IMAGENS/americala.png"
 }
 
 else
-if(hour > 4 && hour <= 5)
+if(hour >= 2 && hour <3)
 {
-document.getElementById("status").src="IMAGENS/ctba.png"
+document.getElementById("status").src="IMAGENS/dhl.png"
 }
 
 else
-if(hour > 5 && hour <= 6)
+if(hour >= 3 && hour < 4)
+{
+document.getElementById("status").src="IMAGENS/vgi.png"
+}
+
+else
+if(hour >= 4 && hour < 5)
+{
+document.getElementById("status").src="IMAGENS/campinas.png"
+}
+
+else
+if(hour >= 5 && hour < 6)
 {
 document.getElementById("status").src="IMAGENS/flp.png"
 }
 
 else
-if(hour > 6 && hour <= 7)
+if(hour >= 6 && hour < 8)
 {
-document.getElementById("status").src="IMAGENS/hub08.png"
+document.getElementById("status").src="IMAGENS/dfgo.png"
 }
 
 else
-if(hour > 12 && hour <= 13)
+if(hour >= 8 && hour < 9)
 {
-document.getElementById("status").src="IMAGENS/hub14.png"
+document.getElementById("status").src="IMAGENS/sedex1.png"
 }
 
 else
-if(hour > 17 && hour <= 19)
+if(hour >= 11 && hour < 14)
+{
+document.getElementById("status").src="IMAGENS/rjmg.png"
+}
+
+else
+if(hour >= 14 && hour < 16)
+{
+document.getElementById("status").src="IMAGENS/sedex2.png"
+}
+
+else
+if(hour >= 15 && hour < 16)
+{
+document.getElementById("status").src="IMAGENS/fintrans.png"
+}
+
+else
+if(hour >= 16  && hour < 17)
+{
+document.getElementById("status").src="IMAGENS/texcourier.png"
+}
+
+else
+if(hour >= 17  && hour < 18)
+{
+document.getElementById("status").src="IMAGENS/gflsp.png"
+}
+
+else
+if(hour >= 18  && hour < 20)
 {
 document.getElementById("status").src="IMAGENS/ctba.png"
 }
 
 else
-if(hour > 19 && hour <= 21)
+if(hour >= 20  && hour < 21)
 {
 document.getElementById("status").src="IMAGENS/gfl1.png"
 }
 
 else
-if(hour > 21 && hour <= 23)
+if(hour >= 21  && hour < 23)
 {
-document.getElementById("status").src="IMAGENS/base.png"
+document.getElementById("status").src="IMAGENS/presto537.png"
 }
-
